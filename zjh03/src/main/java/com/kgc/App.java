56a72a3a1fd2c9a459c1ef2f197e7166cd934fd1
zhushuoyuan_glb <zhushuoyuan_glb@kgc.com>
2020-12-15 10:12:17 +0800

    朱硕源说比

---
+++ zjh03/src/main/java/com/kgc/App.java
@@ -0,0 +1,14 @@
+package com.kgc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("我是朱硕源大水比");
+        System.out.println( "Hello World!" );
+    }
+}
